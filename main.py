ec62a55fa9092bb1275ded8ea5f73107df9625dd
Tanvi Kachakayala <>
2021-05-26 23:10:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+number = 9
+
+guess1 =  int( input("guess a number between 1 and 10 "))
+
+if (guess1 > number ):
+  print( "your guess was too high  guess again")
+elif(guess1 < number ) :
+  print(" your guess was too low guess again")
+else:
+  print("congrats correct!!!!")
+
+
+
+guess2 =  int( input("guess a number between 1 and 10 "))
+
+if (guess2 > number ):
+  print( "your guess was too high  guess again")
+elif(guess2 < number ) :
+  print(" your guess was too low guess again")
+else:
+  print("congrats correct!!!!")
